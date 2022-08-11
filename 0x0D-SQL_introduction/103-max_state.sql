459cd0071e783914592305678e13db245741b12f
calebapril <calebessien001@gmail.com>
2022-08-11 09:53:41 -0700

    testing

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,4 @@
+-- Write a script that displays the max temperature of each state (ordered by State name).
+
+SELECT state, max(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC LIMIT 3;
+
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Write a script that creates a table called first_table in the current database in your MySQL server.
+
+CREATE TABLE first_table (
+  id INT, name VARCHAR(256));
